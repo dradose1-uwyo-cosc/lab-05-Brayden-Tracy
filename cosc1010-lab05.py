e86d8842856bb834511a9b09ed2103df93fcb4c1
Brayden-Tracy <btracy4@uwyo.edu>
2024-10-09 21:49:08 +0000

    Did the assignment

--- cosc1010-lab05.py
+++ cosc1010-lab05.py
@@ -1,9 +1,9 @@
-# Your Name Here
+# Brayden Tracy
 # UWYO COSC 1010
-# Submission Date
+# 10/09/2024
 # Lab 03 
-# Lab Section: 
-# Sources, people worked with, help given to: 
+# Lab Section: 15
+# Sources, people worked with, help given to: google and Brandon
 # your
 # comments
 # here
@@ -95,8 +95,19 @@ min_temps = [
 # The use of len() is fine
 # You can do this in two individual loops, or a single loop if you wish 
 
-print(f"Max temp = {max_temp}")
-print(f"Min temp = {min_temp}")
+max_value = max_temps[0]
+
+for i in range(len(max_temps)):
+    if max_temps[i]>max_value:
+        max_value=max_temps[i]
+print("Maximum value:", max_value)
+
+min_value = min_temps[0]
+
+for i in range(len(min_temps)):
+    if min_temps[i]<min_value:
+        min_value=min_temps[i]
+print("Minimum value:", min_value)
 
 # Given the below list 
 numbers = [-61, -76, 94, 21, 97, -4, 21, 56, -26, 9, 100, 56, -7, -32, 60, -68, -25, 3, -10, -83, 63, 0, 13, -99, 87, -46, -88, -71, 4, -99, -15, -12, 72, -1, -20, -90, 32, -36, -59, 83, 78, 52, 43, 55, 12, 16, -37, -5, -98, -53]
@@ -104,11 +115,26 @@ numbers = [-61, -76, 94, 21, 97, -4, 21, 56, -26, 9, 100, 56, -7, -32, 60, -68, 
 # You should print the number and the result within an f-string 
 # Example output: 83 is positive
 
-print(f'There are {pos_count} positive numbers')
-print(f'There are {neg_count} negative numbers')
-print(f"Zero occurred {zero_count} time(s)")
+positive_count = sum(1 for num in numbers if num > 0)
+print(f'The number of positive numbers in the list is:{positive_count}')
+
+negative_count = sum(1 for num in numbers if num < 0)
+print(f'The number of negative numbers in the list is:{negative_count}')
+
+zero_count = sum(1 for num in numbers if num == 0)
+print(f'The number of zeros in the list is:{zero_count}')
+
 # Given the same numbers list, give the sum of all positive numbers, and the sum of all negative numbers
 # This should be done within a single loop
 
-print(f"Sum of positive numbers {pos_sum}")
-print(f"Sum of negative numbers {neg_sum}")
+sum_positive = 0
+sum_negative = 0
+
+for num in numbers:
+    if num > 0:
+        sum_positive += num 
+    elif num < 0:
+        sum_negative += num 
+
+print("Sum of positive numbers:", sum_positive)
+print("Sum of negative numbers:", sum_negative)
